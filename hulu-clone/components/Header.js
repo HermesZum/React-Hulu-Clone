1b12ad92cc99e3d6f93b91a220d0123761e5d459
Clemente Nogueira <git.3gear@gmail.com>
2022-05-12 19:50:37 +0100

    :sparkles: The HeaderItem component, its logic and styles were created.

--- hulu-clone/components/Header.js
+++ hulu-clone/components/Header.js
@@ -1,9 +1,23 @@
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline";
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 function Header() {
     return (
-        <header className="">
-            <h1>Hulu Clone</h1>
+        <header>
+            <HeaderItem title="HOME" Icon={HomeIcon}/>
+            <HeaderItem title="TRENDING" Icon={LightningBoltIcon}/>
+            <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon}/>
+            <HeaderItem title="COLLECTIONS" Icon={CollectionIcon}/>
+            <HeaderItem title="SEARCH" Icon={SearchIcon}/>
+            <HeaderItem title="ACCOUNT" Icon={UserIcon}/>
             <Image
                 className="object-contain"
                 src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
--- hulu-clone/tailwind.config.js
+++ hulu-clone/tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 480px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '976px',
+      // => @media (min-width: 976px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {},
   },
   plugins: [],
