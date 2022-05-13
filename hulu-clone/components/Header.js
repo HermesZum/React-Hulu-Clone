a3d2c985f7510b2ac370a96b772a94f9a585dfc3
Clemente Nogueira <git.3gear@gmail.com>
2022-05-13 01:32:11 +0100

    :lipstick: The header and its items are created and styled.

--- hulu-clone/components/Header.js
+++ hulu-clone/components/Header.js
@@ -11,13 +11,15 @@ import HeaderItem from "./HeaderItem";
 
 function Header() {
     return (
-        <header>
-            <HeaderItem title="HOME" Icon={HomeIcon}/>
-            <HeaderItem title="TRENDING" Icon={LightningBoltIcon}/>
-            <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon}/>
-            <HeaderItem title="COLLECTIONS" Icon={CollectionIcon}/>
-            <HeaderItem title="SEARCH" Icon={SearchIcon}/>
-            <HeaderItem title="ACCOUNT" Icon={UserIcon}/>
+        <header className="flex flex-col md:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="HOME" Icon={HomeIcon}/>
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon}/>
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon}/>
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon}/>
+                <HeaderItem title="SEARCH" Icon={SearchIcon}/>
+                <HeaderItem title="ACCOUNT" Icon={UserIcon}/>
+            </div>
             <Image
                 className="object-contain"
                 src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
