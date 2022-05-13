c63c3e1587340b33b4491b937790d71fd25ec844
Clemente Nogueira <git.3gear@gmail.com>
2022-05-13 20:18:04 +0100

    :construction: The navigation router has been created and configured.

--- hulu-clone/components/Nav.js
+++ hulu-clone/components/Nav.js
@@ -1,15 +1,16 @@
 import requests from "../utils/requests";
+import { useRouter } from "next/router";
 
 function Nav() {
+    const router = useRouter();
     return (
         <nav className="relative">
-            <div className="
-            flex px-10 md:px-20 text-2xl whitespace-nowrap space-x-10 md:space-x-20 overflow-x-scroll scrollbar-hide
-            ">
+            <div className="flex px-10 md:px-20 text-2xl whitespace-nowrap space-x-10 md:space-x-20 overflow-x-scroll scrollbar-hide">
                 {Object.entries(requests).map(([key, { title, url }]) => (
-                    <h2 key={key} className="
-                    last:pr-5 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500
-                    ">
+                    <h2
+                        key={key}
+                        onClick={() => router.push(`/?genre=${key}`)}
+                        className="last:pr-5 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500">
                         {title}
                     </h2>
                 ))}
